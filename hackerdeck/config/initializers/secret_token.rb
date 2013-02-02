d0e20b2c801f848c10d05a1acd45cfa80e48f2ed
James Hwang <james.hwang.usc@gmail.com>
2013-02-02 12:54:38 -0800

    Init git

---
+++ hackerdeck/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackerdeck::Application.config.secret_token = 'b0e530e7dab7f1af63031b78a8eb4691da2963c2cf63d2bee2bfc67958663abbff6c8f8e7b8e626f95cf156700df07d3dbe71c48ff5e1ccdf187875f8354e8d0'
