7e8551bf24c454bffc42e9062183b6a0a1d1c3d5
James Hwang <james.hwang.usc@gmail.com>
2013-02-02 15:29:59 -0800

    Did ton of stuff, please work

--- hackerdeck/config/routes.rb
+++ hackerdeck/config/routes.rb
@@ -1,4 +1,7 @@
 Hackerdeck::Application.routes.draw do
+  resources :events
+
+  root :to => 'events#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ Hackerdeck::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
