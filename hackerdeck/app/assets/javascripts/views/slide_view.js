35d02be0d35200182f9a5fb7c4d89e7186945e7c
Vincent Samaco <vsamaco@gmail.com>
2013-02-03 03:40:16 -0800

    Moved slide image tag into template

--- hackerdeck/app/assets/javascripts/views/slide_view.js
+++ hackerdeck/app/assets/javascripts/views/slide_view.js
@@ -5,10 +5,17 @@ Hackerdeck.Views.SlideView = Backbone.View.extend({
   initialize: function() {
     this.countdown = new Hackerdeck.Views.SlideCountdownView({model: this.model});
     this.model.on('change', this.render, this);
-    this.model.on('reset', this.render, this);
+    this.collection.on('reset', this.render, this);
   },
 
-  getRenderData: function() {},
+  getRenderData: function() {
+    var current_team_index = this.model.get('current_team');
+    var current_team = this.collection.at(current_team_index);
+    console.log(current_team);
+    return {
+      team: current_team ? current_team.toJSON() : ''
+    }
+  },
 
   render: function() {
     this.$el.html(this.template(this.getRenderData()));
@@ -20,8 +27,8 @@ Hackerdeck.Views.SlideView = Backbone.View.extend({
     var current_team_index = this.model.get('current_team');
     var current_team = this.collection.at(current_team_index);
 
-    if(current_team) {
-      this.$("#countdown-container").html(this.countdown.render().el);
+    this.$("#countdown-container").html(this.countdown.render().el);
+    if(current_team_index) {
       this.$('#image-container').html('<img src="'+ current_team.get('image_url') +'" class="img-rounded" style="max-width:100%;display:block;max-height:100%;margin:0 auto;"/>');
     } else {
 
