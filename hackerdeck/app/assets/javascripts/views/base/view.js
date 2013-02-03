f4177644381bbeaa36572bc03c24b122b7a6d621
Vincent Samaco <vsamaco@gmail.com>
2013-02-02 22:47:56 -0800

    Placeholder for app view

---
+++ hackerdeck/app/assets/javascripts/views/base/view.js
@@ -0,0 +1,16 @@
+Hackerdeck.Views.Base.View = Backbone.View.extend({
+  initialize: function() {
+    this.render = _.bind(this.render, this);
+  },
+
+  template: function() {},
+  getRenderData: function() {},
+
+  render: function() {
+    this.$el.html(this.template(this.getRenderData()));
+    this.afterRender();
+    return this;
+  },
+
+  afterRender: function() {}
+});
