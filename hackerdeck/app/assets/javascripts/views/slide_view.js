255516e68246ebdbf11f28755b93838ccf4dd3e0
Vincent Samaco <vsamaco@gmail.com>
2013-02-03 00:17:24 -0800

    Added dummy image to slide view

--- hackerdeck/app/assets/javascripts/views/slide_view.js
+++ hackerdeck/app/assets/javascripts/views/slide_view.js
@@ -6,7 +6,12 @@ Hackerdeck.Views.SlideView = Backbone.View.extend({
 
   render: function() {
     this.$el.html(this.template(this.getRenderData()));
-    
+    this.afterRender();
     return this;
   },
+
+  afterRender: function() {
+    //this.$("#countdown-container").html(this.countdown.render().el);
+    this.$('#image-container').html('<img src="http://i.imgur.com/oCP7Pom.jpg" class="img-rounded" style="max-width:100%;display:block;max-height:100%;margin:0 auto;"/>');
+  }
 });
