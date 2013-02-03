4f8597aa096c31c59a1e336cbdc0a9ad961e5eef
Vincent Samaco <vsamaco@gmail.com>
2013-02-03 02:05:54 -0800

    Temporary way to render sidebar team list

--- hackerdeck/app/assets/javascripts/views/sidebar_teams_view.js
+++ hackerdeck/app/assets/javascripts/views/sidebar_teams_view.js
@@ -2,11 +2,33 @@ Hackerdeck.Views.SidebarTeamsView = Backbone.View.extend({
   id: 'sidebar-teams-view',
   template: JST['sidebar_teams_view'],
 
-  getRenderData: function() {},
+  initialize: function() {
+    this.collection.on('reset', this.render, this);
+  },
+
+  getRenderData: function() {
+    return {
+      teams: this.collection.toJSON()
+    }
+  },
 
   render: function() {
     this.$el.html(this.template(this.getRenderData()));
-    
+    //this.afterRender();
     return this;
   },
+
+  afterRender: function() {
+    //this.$('.team-list').html('AFTER');
+  },
+
+  addTeams: function() {
+    this.$('.team-list').empty();
+    this.collection.forEach(this.addTeam);
+  },
+
+  addTeam: function(team) {
+    console.log('add team:' + team.get('name'));
+    this.$('.team-list').html('<li>' + team.get('name') + '</li>');
+  }
 });
