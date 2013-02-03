3601fc73f876ffa6a161f8019b728e73a958abc8
Vincent Samaco <vsamaco@gmail.com>
2013-02-03 00:03:18 -0800

    Created slide view and fixed css for client only

--- hackerdeck/app/assets/javascripts/views/app_view.js
+++ hackerdeck/app/assets/javascripts/views/app_view.js
@@ -2,11 +2,20 @@ Hackerdeck.Views.AppView = Backbone.View.extend({
   id: 'app-view',
   template: JST['app_view'],
 
+  initialize: function() {
+    this.slideView = new Hackerdeck.Views.SlideView();
+  },
+
   getRenderData: function() {},
 
   render: function() {
     this.$el.html(this.template(this.getRenderData()));
-    
+    this.afterRender();
     return this;
   },
+
+  afterRender: function() {
+    console.log('after render');
+    this.$('#slide-container').html(this.slideView.render().el);
+  }
 });
